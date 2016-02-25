70d5dff6ed4eb2ae17cb8766cf3bcf3f15cf3d00
Evan Dorsky <evan.dorsky@icloud.com>
2016-02-25 12:48:44 -0500

    Talking to IO expander

--- i2ctest/i2ctest.c
+++ i2ctest/i2ctest.c
@@ -12,4 +12,20 @@ int16_t main(void) {
     init_ui();
     init_pin();
     init_i2c();
+
+    led_on(&led3);
+
+    i2c_open(&i2c3, 1e3);
+    i2c_start(&i2c3);
+    i2c_putc(&i2c3, 0x4E);
+    i2c_idle(&i2c3);
+    i2c_putc(&i2c3, 0xF4);
+    i2c_idle(&i2c3);
+    i2c_putc(&i2c3, 0xF4);
+    i2c_idle(&i2c3);
+    i2c_putc(&i2c3, 0xF4);
+    i2c_stop(&i2c3);
+    i2c_close(&i2c3);
+
+    while(1) {}
 }
