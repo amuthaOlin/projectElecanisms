90ee4241391c7665b3c210017c44eb86bd64e2a7
Evan Dorsky <evan.dorsky@icloud.com>
2016-03-24 13:35:52 -0400

    Initial space comms header file

---
+++ lib/spacecomms.h
@@ -0,0 +1,13 @@
+#ifndef _SPACECOMMS_H
+#define _SPACECOMMS_H
+
+#include <stdint.h>
+
+typedef struct SPACK_DIR {
+  unsigned packet:2;
+  unsigned actaddr:5;
+  unsigned actact:6;
+  unsigned :19;
+} SPACK_DIR;
+
+#endif
