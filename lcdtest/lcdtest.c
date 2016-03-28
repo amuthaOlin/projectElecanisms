2d0296cce4b8821e09be38de2d3ed26a05ede082
Evan Dorsky <evan.dorsky@icloud.com>
2016-03-28 10:56:24 -0400

    Lots of build system changes

---
+++ lcdtest/lcdtest.c
@@ -0,0 +1,45 @@
+#include <p24FJ128GB206.h>
+#include <stdint.h>
+#include "config.h"
+#include "common.h"
+#include "ui.h"
+#include "pin.h"
+#include "i2c.h"
+#include "timer.h"
+#include "lcd.h"
+
+// I2C Reg (MSB) P7 P6 P5 P4 P3 P2 P1 P0
+// Driver pin    D7 D6 D5 D4 ?  E  RW RS
+
+int16_t main(void) {
+    init_clock();
+    init_pin();
+    init_i2c();
+    init_timer();
+    init_ui();
+    init_delay();
+    // led_on(&led3);
+    led_off(&led1);
+    led_on(&led1);
+
+    init_lcd();
+    led_on(&led2);
+
+    lcd_write(&lcd1, 'S');
+    lcd_write(&lcd1, 'P');
+    lcd_write(&lcd1, 'A');
+    lcd_write(&lcd1, 'C');
+    lcd_write(&lcd1, 'E');
+    lcd_write(&lcd1, ' ');
+    lcd_write(&lcd1, 'T');
+    lcd_write(&lcd1, 'E');
+    lcd_write(&lcd1, 'A');
+    lcd_write(&lcd1, 'M');
+    lcd_write(&lcd1, '!');
+    i2c_stop(lcd1.i2c);
+
+    while(1) {
+        delayMicroseconds(500);
+        led_toggle(&led2);
+    }
+}
