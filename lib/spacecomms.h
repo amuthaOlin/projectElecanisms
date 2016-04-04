ec1830af86d57d4770e4c74296235941fff24659
Evan Dorsky <evan.dorsky@icloud.com>
2016-04-04 12:24:47 -0400

    Update console state variables

--- lib/spacecomms.h
+++ lib/spacecomms.h
@@ -21,17 +21,15 @@ typedef struct CONS1_STATE {
   unsigned long :22;
 } CONS1_STATE;
 
-uint8_t CONS1_NUMACTS = 9;
+uint8_t CONS1_NUMACTS = 7;
 uint8_t CONS1_STATES[] = {
-    1,
-    2,
-    2,
-    2,
-    2,
-    2,
-    2,
-    1,
-    1
+    1, // red_button:1;
+    2, // toggle1:1;
+    2, // toggle2:1;
+    8, // joystick:4;
+    1, // wormhole1:1;
+    1, // wormhole2:1;
+    2  // hotsystem:1; 
 }
 
 typedef struct CONS2_STATE {
@@ -51,19 +49,21 @@ typedef struct CONS2_STATE {
   unsigned long :17;
 } CONS2_STATE;
 
-uint8_t CONS2_NUMACTS = 11;
+uint8_t CONS2_NUMACTS = 13;
 uint8_t CONS2_STATES[] = {
-    1,
-    2,
-    2,
-    2,
-    2,
-    2,
-    0,
-    1,
-    1,
-    3,
-    4
+    1, // red_button:1;
+    1, // green_button1:1;
+    1, // green_button2:1;
+    1, // green_button3:1;
+    1, // green_button4:1;
+    1, // wormhole1:1;
+    1, // wormhole2:1;
+    2, // tri_state:2;
+    4, // slider:2;
+    2, // hotsystem1:1;
+    2, // hotsystem2:1;
+    2, // hotsystem3:1;
+    2  // hotsystem4:1;
 }
 
 typedef struct CONS3_STATE {
@@ -78,17 +78,16 @@ typedef struct CONS3_STATE {
   unsigned long :24;
 } CONS3_STATE;
 
-uint8_t CONS3_NUMACTS = 9;
+uint8_t CONS3_NUMACTS = 8;
 uint8_t CONS3_STATES[] = {
-    1,
-    2,
-    2,
-    1,
-    1,
-    1,
-    0,
-    1,
-    1
+    1, // red_button:1;
+    1, // triangle1:1;
+    1, // triangle2:1;
+    1, // triangle3:1;
+    2, // toggle1:1;
+    2, // toggle2:1;
+    1, // wormhole1:1;
+    1  // wormhole2:1;
 }
 
 #endif
