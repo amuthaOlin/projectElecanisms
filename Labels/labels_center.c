2421583d64f003542edf5500999c2a398f5e81f2
Erin Pierce <erinpierce13@gmail.com>
2016-04-18 14:18:56 -0400

    Refactored into two files

---
+++ Labels/labels_center.c
@@ -0,0 +1,232 @@
+// Theme 0
+char[100][17] lab_general={"Engine", "Gyrator", "Flame Thrower", "Delwelder", "Fibrillator", "Generator", "Dismantler", "Transponder", "Collider", "Transformer", "Controller", "Vaporizer", "Diffuser", "Amplifier", "Combustor", "Extruder", "Silencer", "Detonator", "Compressor", "Contraption", "Deflector", "Simulator", "Isomorph", "Defibulator ", "Accelerator", "Replicator", "Differential", "Sabilizer", "Indicator", "Defacer", "Distiller", "Elevator", "Engtangler", "Airlock", "Stopcock", "De-icer", "Mulcher", "Grindr", "Blinder", "Hoover", "Mincher", "Gedder", "Gamma Ray", "Engorger", "Jumper", "Spaceball", "Barge", "Breaks", "Thrusters", "Shunter", "Flywheel", "Flux Beam", "Extra Nipple", "Gamma Punch", "Pocket Bulb", "Death Slicer", "Lambda Flange", "U-Alert", "Warp Hoop", "Ultra Grill", "Eigen Crank", "Clutch Shifter", "Sub Network", "Lube Hose", "Ambiwing", "Beta-Pulsar", "Pico Plucker", "Psi Kick", "Radio Trellis", "Astro Mulcher", "Multiphaser", "Delta Hole", "Giga Bellows", "Cycloscope", "B-Fold", "Depth Synth", "Vibro Hitch", "Shift Baffle", "F-Whisker", "Extra Flush", "G-Spigot", "Gyro Peg", "Rear Frustrum", "O-Lube", "Vibro Ignition", "Orb Sphere", "Sub Tensor", "Cyclo Cube", "Lunar Canal", "Fibre Scrubber", "Terra Cover", "Theta Bypass", "Stun Rockets", "Grip Horn", "Technobraid", "P-Satellite", "Volt Puncher", "Giga Paddler", "Hypno Core", "Macro Slicer"};
+// Theme 1
+char[46][17] lab_long={"Spark Scrambler", "Clutch Catalyst", "Suction Scissor", "Epsilon Counter", "Photo Sprinkler", "Depth Sanitizer", "Lambda Lubricant", "Phono Structure", "Omicron Control", "Zeta-Ventricle ", "Flange Aperture", "Beta-Substrate", "Phase Collapser", "Pseudo Register", "Chroma Scratcher", "Spectro Scanner", "Delta Harmonizer", "Chlorosplitter", "Suction Compass", "Power Substrate", "Vibro Lubricant", "Nipple Membrane", "Plasma Sanitizer", "Psi Synthesizer", "Spectrum Conduit", "Kappa Ventricle", "Epsilon Shifter", "Iota Transporter", "Plectrotumbler", "Hydromechanism", "Chroma Irritant", "Omicron Crystal", "Fibre Sprinkler", "Mirror Manifold", "Spectro Capsule", "Wrench Dampener", "Power Trapezoid", "Ribbon Lubricant", "Plasma Solenoid", "Exlposion Sphere", "Flange Cartridge", "Mezzo-Restraint", "Photon Collapser", "Omicron Mulcher", "Synth Harmonizer", "Spine Scratcher"};
+// Theme 2
+char[46][17] lab_homo={"Baud Tuner", "Bawdy Tuner", "Shea Butterer", "She Butterer", "Lie Detector", "Lye Detector", "Cocoa Eater", "Coco Eater", "Whale Tail", "Whale Tale", "Cheap Part", "Cheep Part", "Rap string", "Wrap string", "Beer Brew", "Beer Bruise", "Meet Peg", "Meat Peg", "Pea Pod", "Pee Pod", "Loot Booty", "Lute Booty", "Swtich sign", "Switch sine", "Wax Ceiling", "Wax Sealing", "Anti-Bertha", "Auntie Bertha", "Lip Bomb", "Lip Balm", "Planar Surface", "Planner surface", "Tacks Board", "Tax Board", "Elicit feelings", "Illicit feelings", "Gorilla War", "Guerilla War", "Happy Days", "Happy Daze", "Flee Horn", "Flea Horn", "Naval Treaty", "Navel Treaty", "Second Storey", "Second Story"};
+// Theme 3
+char[46][17] lab_sym={"~*&%%", "*****", "$%%^@", ")*@$", "%%@%%&", "@$%%@%%", "**$$$", "!#$%%#", "#$^&@", ")%%^*(", "^&*@$", "_(#%%!#", "`<>:'@", "?????", "?#$%%!", "':[}=", "':<(*", "?|}+", "^$~@", "`,?}", "&%%@!", "-=&*", "//:\\", "><><>", "A##$$?", "&$^#", ")+-_#", "@#^$", "()_^^", "#$&*", "&&&&&", "()_^+", "!#%%", "&%%$#", "#####", "!<>{", "{$#%%", ":+*&", "<?#$", "|:{<", "$^@&", "<|}:{", "@$&!", "&+_%%#", "&#%%+", "|:))!#"};
+// Theme 4
+char[46][17] lab_emo={":-(", "(*^.^*)", "(^_^)", "#^_^#", "n__ny", "(*^@^*)", "~~>_<~~", "^_~", "*o*  :}", ":P  *o*", "@_@", "<@_@>", "o_O", "^o^", "O_O", "-_-", "^_^", ":D  :\\", "*o*  (:", "8) *o*", ":}  :-{o", "X-D", ":-))", ">:[", ":-(", ":^*", ":\\ </3", "%-}", "<*)))-{", "*<|:-)", "<3  :-)", "(>_<)", "((+_+))", "('_')", "(T_T)", "(=_=)", "~~~ ) \'>~", "(=^.^=)", "\\ (^o^) /", "(*_*)", ":-(  ;-)", ";-p :-3", ";-) :-###", ">^_^<", "(._.)", "~(_8^(|)"};
+// Theme 5
+char[46][17] lab_num={"734", "613", "709", "163", "666", "472", "920", "375", "649", "263", "255", "8573", "9384", "4568", "7345", "1256", "8763", "2345", "1432", "8546", "2366", "12453", "23561", "17357", "24627", "79425", "24564", "28654", "20492", "28935", "16346", "90646", "20492", "34626", "97643", "56674", "11264", "34789", "12345", "35670", "155362", "264473", "970024", "566374", "987774", "234617"};
+// Theme 6
+char[46][17] lab_cons={"mqhc","lyvj","dchy","kdfg","zqht","xzyl","chdn","pgmr","mwdf","mfcj","wjccx","wftnb","tgldt","zdqgt","mftgy","jklbf","rsxlk","lrytm","myfkz","sfwdr","hlwsg","jmfdf","xzxyl","lvfqx","lpkbb","jjxxn","ysdff","nfqfj","gyjys","jmvsw","ndgbtv","lgxfqk","kdhmpr","vjgdqp","dghxqd","xsxdjj","zylzvx","jbzbrs","jmkvqk","hyqwvk","gbzfsgs","tlhwwgj","tgpvgsk","nrvgvvb","gxgvhxl","bxphqdm"};
+// Theme 7
+char[46][17] lab_adven={"Nightosphere", "Prismos Pickles", "Hambo", "Shame Prison", "MeeMow", "Treetrunks", "Bananna Guard", "Peppermint Butler", "Treehouse", "BMO", "Angelface", "Glorb", "Mushroom", "Litch", "Simon", "Gunther No", "Neddy", "Fan Fiction", "Fries", "The Farm", "Canyon", "Grass Sword", "Susan Strong", "Science", "LSP", "The Maid", "James Baxter", "Destiny Gang", "Ranicorn", "Tiffany", "Graybles", "Party Pat", "Enchiridion", "Lemongrab", "Gumball Guardian", "Goliad", "Bacon Pancakes", "Dungeon Train", "Lumpy Space", "Super Porp", "Ax Bass", "Mind Games", "Demonic Eye", "Milk Suit", "Sentient Sandwich", "Neptr"};
+// Theme 8
+char[46][17] lab_wars={"Deathstar", "X-Wing", "TIE Fighter", "Alderaan", "Coruscant", "Dagobah", "Endor", "Hoth", "Empire", "Jakku", "Rebel Alliance", "Naboo", "Starkiller", "Tatooine", "The Force", "Kylo Ren", "Rey", "Finn", "Skywalker", "Falcon", "Han Solo", "Yoda", "Princess Leia", "Jabba the Hut", "Wookies", "Ewoks", "Darth Vader", "Daddy Issues", "Darth Maul", "Palpatine", "Star Destroyer", "Landspeeder", "C-3PO", "BB-8", "R2-D2", "Lightsaber", "The Light Side", "The Dark Side", "Clone", "Chewbacca", "First Order", "It's a Trap!", "Hosian Prime", "POE", "Resistance", "AT-AT"};
+// Theme 9
+char[46][17] lab_pir={"Treasure", "Galleon", "Black Beard", "Carribean", "Tortuga", "Jack Sparrow", "Black Pearl", "Davvy Jones", "Islands", "Spiced Rum", "Rum Gone", "Starboard", "Port", "Mast", "Rigging", "Plank", "Keel Haul", "Anchor", "Bermuda", "Avast", "Shiver Timbers", "Monkey", "Poop Deck", "Cockswain", "Landlubber", "Chips Ahoy", "Batten Hatches", "Booty", "Crows Nest", "Jolly Roger", "Man-O-War", "Scallyway", "Calico Jack", "Captian Kidd", "Barbary Coast", "Cat O Nine", "Cutlass", "Cannon", "Powder Chest", "Kraken", "Parrot", "Wooden Leg", "Captian Hook", "Eye Patch", "Dread Pirate Roberts", "Hydra"};
+
+int[9] theme_len{100,46,46,46,46,46,46,46,46};
+int[3] lab_len={2,8,7};
+int[2] easy_lab={0,1};
+int[4] med_lab={0,1,2,4,5,7,8,9};
+int[6] hard_lab={0,1,2,3,4,5,6};
+char[9][33] messages {" ","Impending Didactic Aberation","They're over there, by their ship","Tr@n$|at0r M@lfun(#ion","Lol good luck (^_^)/* ttyl, bai","Can I Ha Your Numba","Thngs r brkng dwn","Come on grab your friends!","May the force be with you","ARRG, spaceship now be a seaship"};
+
+//void (*mods[8]) (char* str, uint8_t freq, uint8_t shift) ={Nada,CamelCase,Brnrd,Missing,Leet,Pig,Reverse,Scramble,Caesar};
+
+
+
+
+typedef struct _LEVEL {
+    // constant
+    uint8_t[8] asteroids;  
+    uint8_t[8] wormholes;
+    uint8_t[6] label1;
+    uint8_t[6] label2;
+    uint8_t[6] label3;
+    uint8_t lab_theme;
+    uint8_t mod;
+    uint8_t arg_freq;
+    uint8_t arg_shift;
+    float level_time;
+    float cmd_time;
+    uint8_t[6] actuators;
+    char[33] message;
+
+} _LEVEL;
+
+
+void level_init(_LEVEL level){
+	level->asteroids={0,0,0,0,0,0,0,0};  
+    level->wormholes={0,0,0,0,0,0,0,0};  
+    level->label1={0,0,0,0,0,0};
+    level->label2={0,0,0,0,0,0};
+    level->label3={0,0,0,0,0,0};
+    level->lab_theme=0;
+    level->mod=0;
+    level->arg_freq=0;
+    level->arg_shift=0;
+    level->level_time=0;
+    level->cmd_time=0;
+    level->actuators={0,0,0,0,0,0};
+    level->message={"                                "};
+}
+
+void pick_labels(_LEVEL level uint8_t theme, uint8_t num){
+	// console 1
+	uint8_t i=0;
+	while (i<num){
+		level->lables1[i]=randint(0,theme_len[theme]); // populate random labels
+		i++;
+	}
+	// console 2
+	i=0;
+	while (i<num){
+		level->lables2[i]=randint(0,theme_len[theme]); // populate random labels
+		i++;
+	}
+	// console 3
+	i=0;
+	while (i<num){
+		level->lables3[i]=randint(0,theme_len[theme]); // populate random labels
+		i++;
+	}
+}
+
+void level_setup(uint8_t lev_num, _LEVEL level){
+	if (lev_num == 1){ // First level has no complications
+		level->lab_theme=0;
+		level->level_time=325;
+		level->cmd_time=20;
+		level->actuators=[1,1,1,1,0,0]; // Only use 4 buttons
+		pick_labels(level,0,4);
+	}
+	else if (lev_num ==2){ // Second Level
+		level->lab_theme=0;
+		level->level_time=300;
+		level->cmd_time=19;
+		level->actuators=[1,1,1,1,1,1]; // Use all buttons
+		pick_labels(level,0,6);
+	}
+	else if (lev_num ==3){ // Third Level
+		level->lab_theme=0;
+		level->level_time=275;
+		level->cmd_time=18;
+		level->actuators={1,1,1,1,1,1}; // Use all buttons
+		level->asteroids[0]=randint(4,10); // Seed two asteroids
+		level->asteroids[1]=randint(12,20);
+		level->message="Shake ball to avoid asteroids"
+		pick_labels(level,0,6);
+	}
+
+	else if (lev_num ==4 ){ // Level 4 (tier 2)
+		level->lab_theme=0;
+		level->level_time=250;
+		level->cmd_time=17;
+		level->actuators=[1,1,1,1,1,1]; // Use all buttons
+		level->asteroids[0]=randint(10,35); // Seed an asteroid
+		level->wormholes[0]=randint(0,5); // Seed two wormholes
+		level->wormholes[1]=randint(20,50);
+		level->message="Press flip to escape wormwhole"
+		pick_labels(level,theme,6);
+	}
+
+	else if (lev_num <7){ // Tier 2 levels
+		uint8_t theme=zero_prb(randint(1,lab_len[0]),70,0) // 70% chance of default
+		level->lab_theme=easy_lab[theme];
+		level->level_time=250;
+		level->cmd_time=17;
+		level->actuators=[1,1,1,1,1,1]; // Use all buttons
+		level->asteroids[0]=randint(0,15); // Seed two asteroids
+		level->asteroids[1]=randint(30,45);
+		level->wormholes[0]=randint(15,30);// Seed two wormholes
+		level->wormholes[1]=randint(30,45);
+		level->message=messages[theme];
+		pick_labels(level,theme,6);
+		level->mod=zero_prob(randint(0,3),50,0};
+		level->arg_freq=randint(5,10);
+
+	}
+
+	else if (lev_num <10){ // Tier 3 levels
+		uint8_t theme=zero_prb(randint(1,lab_len[1]),50,0)// 50% chance of default
+		level->lab_theme=easy_lab[theme];
+		level->level_time=225;
+		level->cmd_time=16;
+		level->actuators=[1,1,1,1,1,1]; // Use all buttons
+		level->asteroids[0]=randint(0,10); // Seed three asteroids
+		level->asteroids[1]=randint(20,30);
+		level->asteroids[2]=randint(30,50);
+		level->wormholes[0]=randint(15,30);// Seed two wormholes
+		level->wormholes[1]=randint(30,45);
+		level->message=messages[theme];
+		pick_labels(level,theme,6);
+		level->mod=zero_prob(randint(0,8),50,0};
+		level->arg_shift=1; // Make them easy
+		level->arg_freq=randint(5,8);
+	}
+
+	else if (lev_num <14){ // Tier 4 levels
+		uint8_t theme=zero_prb(randint(0,lab_len[1]),40,0)
+		level->lab_theme=easy_lab[theme];
+		level->level_time=200;
+		level->cmd_time=15;
+		level->actuators=[1,1,1,1,1,1]; // Use all buttons
+		level->asteroids[0]=randint(0,10); // Seed three asteroids
+		level->asteroids[1]=randint(20,30);
+		level->asteroids[2]=randint(30,50);
+		level->wormholes[0]=randint(0,20);// Seed three wormholes
+		level->wormholes[1]=randint(20,30);
+		level->wormholes[2]=randint(30,45);
+		level->message=messages[theme];
+		pick_labels(level,theme,6);
+		level->mod=zero_prob(randint(0,8),30,0};
+		level->arg_shift=1; // Make them med
+		level->arg_freq=randint(4,6);
+	}
+
+	else if (lev_num <16){ // Tier 5 levels
+		uint8_t theme=zero_prb(randint(0,lab_len[2]),30,0)
+		level->lab_theme=easy_lab[theme];
+		level->level_time=175;
+		level->cmd_time=14;
+		level->actuators=[1,1,1,1,1,1]; // Use all buttons
+		level->asteroids[0]=randint(0,10); // Seed four asteroids
+		level->asteroids[1]=randint(20,30);
+		level->asteroids[2]=randint(30,40);
+		level->asteroids[3]=randint(40,50);
+		level->wormholes[0]=randint(0,10);// Seed four wormholes
+		level->wormholes[1]=randint(20,30);
+		level->wormholes[2]=randint(30,40);
+		level->wormholes[3]=randint(40,50);
+		level->message=messages[theme];
+		pick_labels(level,theme,6);
+		level->mod=zero_prob(randint(0,8),30,0};
+		level->arg_shift=3; // Make them hard
+		level->arg_freq=randint(3,6);
+	}
+}
+
+uint8_t randint(uint8_t low, uint8_t high){
+	// generate a random number between low and high (inclusive)
+}
+
+uint8_t zero_prob(uint16_t num, uint16_t prob, uint8_t replace){
+	// maybe change a num to replace
+	// based on the probability
+}
+void zero_prob_list(int* start, uint8_t prob, uint8_t replace){
+	// maybe replace every entry in an array with some other number. 
+	// maybe ist the probablity
+	// calls zero_prob
+}
+
+void random_list(int* start, uint8_t len, uint8_t low, uint8_t high){
+	// calls randint in a loop, makes a list
+	// List has to be initialized outsited function
+}
+
+
+void send_level(_LEVEL level, uint8_t console){
+	//Send to appropriate Console:
+	// - label1
+	// - lab_theme
+	// - mod
+	// - arg_freq
+	// - arg_shift
+}
+
---
+++ Labels/labels_console.c
@@ -0,0 +1,73 @@
+// Theme 0
+char[100][17] lab_general={"Engine", "Gyrator", "Flame Thrower", "Delwelder", "Fibrillator", "Generator", "Dismantler", "Transponder", "Collider", "Transformer", "Controller", "Vaporizer", "Diffuser", "Amplifier", "Combustor", "Extruder", "Silencer", "Detonator", "Compressor", "Contraption", "Deflector", "Simulator", "Isomorph", "Defibulator ", "Accelerator", "Replicator", "Differential", "Sabilizer", "Indicator", "Defacer", "Distiller", "Elevator", "Engtangler", "Airlock", "Stopcock", "De-icer", "Mulcher", "Grindr", "Blinder", "Hoover", "Mincher", "Gedder", "Gamma Ray", "Engorger", "Jumper", "Spaceball", "Barge", "Breaks", "Thrusters", "Shunter", "Flywheel", "Flux Beam", "Extra Nipple", "Gamma Punch", "Pocket Bulb", "Death Slicer", "Lambda Flange", "U-Alert", "Warp Hoop", "Ultra Grill", "Eigen Crank", "Clutch Shifter", "Sub Network", "Lube Hose", "Ambiwing", "Beta-Pulsar", "Pico Plucker", "Psi Kick", "Radio Trellis", "Astro Mulcher", "Multiphaser", "Delta Hole", "Giga Bellows", "Cycloscope", "B-Fold", "Depth Synth", "Vibro Hitch", "Shift Baffle", "F-Whisker", "Extra Flush", "G-Spigot", "Gyro Peg", "Rear Frustrum", "O-Lube", "Vibro Ignition", "Orb Sphere", "Sub Tensor", "Cyclo Cube", "Lunar Canal", "Fibre Scrubber", "Terra Cover", "Theta Bypass", "Stun Rockets", "Grip Horn", "Technobraid", "P-Satellite", "Volt Puncher", "Giga Paddler", "Hypno Core", "Macro Slicer"};
+// Theme 1
+char[46][17] lab_long={"Spark Scrambler", "Clutch Catalyst", "Suction Scissor", "Epsilon Counter", "Photo Sprinkler", "Depth Sanitizer", "Lambda Lubricant", "Phono Structure", "Omicron Control", "Zeta-Ventricle ", "Flange Aperture", "Beta-Substrate", "Phase Collapser", "Pseudo Register", "Chroma Scratcher", "Spectro Scanner", "Delta Harmonizer", "Chlorosplitter", "Suction Compass", "Power Substrate", "Vibro Lubricant", "Nipple Membrane", "Plasma Sanitizer", "Psi Synthesizer", "Spectrum Conduit", "Kappa Ventricle", "Epsilon Shifter", "Iota Transporter", "Plectrotumbler", "Hydromechanism", "Chroma Irritant", "Omicron Crystal", "Fibre Sprinkler", "Mirror Manifold", "Spectro Capsule", "Wrench Dampener", "Power Trapezoid", "Ribbon Lubricant", "Plasma Solenoid", "Exlposion Sphere", "Flange Cartridge", "Mezzo-Restraint", "Photon Collapser", "Omicron Mulcher", "Synth Harmonizer", "Spine Scratcher"};
+// Theme 2
+char[46][17] lab_homo={"Baud Tuner", "Bawdy Tuner", "Shea Butterer", "She Butterer", "Lie Detector", "Lye Detector", "Cocoa Eater", "Coco Eater", "Whale Tail", "Whale Tale", "Cheap Part", "Cheep Part", "Rap string", "Wrap string", "Beer Brew", "Beer Bruise", "Meet Peg", "Meat Peg", "Pea Pod", "Pee Pod", "Loot Booty", "Lute Booty", "Swtich sign", "Switch sine", "Wax Ceiling", "Wax Sealing", "Anti-Bertha", "Auntie Bertha", "Lip Bomb", "Lip Balm", "Planar Surface", "Planner surface", "Tacks Board", "Tax Board", "Elicit feelings", "Illicit feelings", "Gorilla War", "Guerilla War", "Happy Days", "Happy Daze", "Flee Horn", "Flea Horn", "Naval Treaty", "Navel Treaty", "Second Storey", "Second Story"};
+// Theme 3
+char[46][17] lab_sym={"~*&%%", "*****", "$%%^@", ")*@$", "%%@%%&", "@$%%@%%", "**$$$", "!#$%%#", "#$^&@", ")%%^*(", "^&*@$", "_(#%%!#", "`<>:'@", "?????", "?#$%%!", "':[}=", "':<(*", "?|}+", "^$~@", "`,?}", "&%%@!", "-=&*", "//:\\", "><><>", "A##$$?", "&$^#", ")+-_#", "@#^$", "()_^^", "#$&*", "&&&&&", "()_^+", "!#%%", "&%%$#", "#####", "!<>{", "{$#%%", ":+*&", "<?#$", "|:{<", "$^@&", "<|}:{", "@$&!", "&+_%%#", "&#%%+", "|:))!#"};
+// Theme 4
+char[46][17] lab_emo={":-(", "(*^.^*)", "(^_^)", "#^_^#", "n__ny", "(*^@^*)", "~~>_<~~", "^_~", "*o*  :}", ":P  *o*", "@_@", "<@_@>", "o_O", "^o^", "O_O", "-_-", "^_^", ":D  :\\", "*o*  (:", "8) *o*", ":}  :-{o", "X-D", ":-))", ">:[", ":-(", ":^*", ":\\ </3", "%-}", "<*)))-{", "*<|:-)", "<3  :-)", "(>_<)", "((+_+))", "('_')", "(T_T)", "(=_=)", "~~~ ) \'>~", "(=^.^=)", "\\ (^o^) /", "(*_*)", ":-(  ;-)", ";-p :-3", ";-) :-###", ">^_^<", "(._.)", "~(_8^(|)"};
+// Theme 5
+char[46][17] lab_num={"734", "613", "709", "163", "666", "472", "920", "375", "649", "263", "255", "8573", "9384", "4568", "7345", "1256", "8763", "2345", "1432", "8546", "2366", "12453", "23561", "17357", "24627", "79425", "24564", "28654", "20492", "28935", "16346", "90646", "20492", "34626", "97643", "56674", "11264", "34789", "12345", "35670", "155362", "264473", "970024", "566374", "987774", "234617"};
+// Theme 6
+char[46][17] lab_cons={"mqhc","lyvj","dchy","kdfg","zqht","xzyl","chdn","pgmr","mwdf","mfcj","wjccx","wftnb","tgldt","zdqgt","mftgy","jklbf","rsxlk","lrytm","myfkz","sfwdr","hlwsg","jmfdf","xzxyl","lvfqx","lpkbb","jjxxn","ysdff","nfqfj","gyjys","jmvsw","ndgbtv","lgxfqk","kdhmpr","vjgdqp","dghxqd","xsxdjj","zylzvx","jbzbrs","jmkvqk","hyqwvk","gbzfsgs","tlhwwgj","tgpvgsk","nrvgvvb","gxgvhxl","bxphqdm"};
+// Theme 7
+char[46][17] lab_adven={"Nightosphere", "Prismos Pickles", "Hambo", "Shame Prison", "MeeMow", "Treetrunks", "Bananna Guard", "Peppermint Butler", "Treehouse", "BMO", "Angelface", "Glorb", "Mushroom", "Litch", "Simon", "Gunther No", "Neddy", "Fan Fiction", "Fries", "The Farm", "Canyon", "Grass Sword", "Susan Strong", "Science", "LSP", "The Maid", "James Baxter", "Destiny Gang", "Ranicorn", "Tiffany", "Graybles", "Party Pat", "Enchiridion", "Lemongrab", "Gumball Guardian", "Goliad", "Bacon Pancakes", "Dungeon Train", "Lumpy Space", "Super Porp", "Ax Bass", "Mind Games", "Demonic Eye", "Milk Suit", "Sentient Sandwich", "Neptr"};
+// Theme 8
+char[46][17] lab_wars={"Deathstar", "X-Wing", "TIE Fighter", "Alderaan", "Coruscant", "Dagobah", "Endor", "Hoth", "Empire", "Jakku", "Rebel Alliance", "Naboo", "Starkiller", "Tatooine", "The Force", "Kylo Ren", "Rey", "Finn", "Skywalker", "Falcon", "Han Solo", "Yoda", "Princess Leia", "Jabba the Hut", "Wookies", "Ewoks", "Darth Vader", "Daddy Issues", "Darth Maul", "Palpatine", "Star Destroyer", "Landspeeder", "C-3PO", "BB-8", "R2-D2", "Lightsaber", "The Light Side", "The Dark Side", "Clone", "Chewbacca", "First Order", "It's a Trap!", "Hosian Prime", "POE", "Resistance", "AT-AT"};
+// Theme 9
+char[46][17] lab_pir={"Treasure", "Galleon", "Black Beard", "Carribean", "Tortuga", "Jack Sparrow", "Black Pearl", "Davvy Jones", "Islands", "Spiced Rum", "Rum Gone", "Starboard", "Port", "Mast", "Rigging", "Plank", "Keel Haul", "Anchor", "Bermuda", "Avast", "Shiver Timbers", "Monkey", "Poop Deck", "Cockswain", "Landlubber", "Chips Ahoy", "Batten Hatches", "Booty", "Crows Nest", "Jolly Roger", "Man-O-War", "Scallyway", "Calico Jack", "Captian Kidd", "Barbary Coast", "Cat O Nine", "Cutlass", "Cannon", "Powder Chest", "Kraken", "Parrot", "Wooden Leg", "Captian Hook", "Eye Patch", "Dread Pirate Roberts", "Hydra"};
+
+int[9] theme_len{100,46,46,46,46,46,46,46,46};
+
+char**[9] level_list ={lab_general,lab_long,lab_homo,lab_sym,lab_emo,lab_num,lab_cons,lab_adven,lab_wars,lab_pir};
+
+void (*mods[8]) (char* str, uint8_t freq, uint8_t shift) ={Nada,CamelCase,Brnrd,Missing,Leet,Pig,Reverse,Scramble,Caesar};
+
+
+typedef struct _LEVEL {
+    // constant
+    uint8_t[6] lab_num;
+    char[6][17] lab_str;
+    uint8_t lab_theme;
+    uint8_t mod;
+    uint8_t arg_freq;
+    uint8_t arg_shift;
+
+} _LEVEL;
+
+void level_init(_LEVEL level){ 
+    level->lab_num={0,0,0,0,0,0};
+    level->lab_str={" "," "," "," "," "," "}
+    level->lab_theme=0;
+    level->mod=0;
+    level->arg_freq=0;
+    level->arg_shift=0;
+}
+
+void label_setup(_LEVEL level){
+	char[17] string;
+	char* stringp = string;
+	uint8_t i=0;
+	while(i<7){
+		string=(level_list[level->lab_theme])[label->lab_num[i]];
+		mod[level->mod](stringp,level->arg_freq, level->arg_shift);
+		label->lab_str[i]=string;
+		i++;
+	}
+}
+
+void label_print(_LEVEL level){
+	// Do printing things
+}
+
+void recieve_level(_LEVEL level, uint8_t console){
+	//Unpack into struct:
+	// - label1
+	// - lab_theme
+	// - mod
+	// - arg_freq
+	// - arg_shift
+}
+
